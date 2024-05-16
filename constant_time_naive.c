68ed96553e83ab6b84b291bd9189e1dda0ed18ae
RasmusRoss <rnylandsted@gmail.com>
2024-05-16 11:39:41 +0200

    added test files

---
+++ constant_time_naive.c
@@ -0,0 +1,19 @@
+
+# include <stdlib.h>
+# include <stdbool.h>
+# include <stdio.h>
+
+uint32_t select_u32 (bool b, uint32_t x, uint32_t y) {
+    return b ? x : y;
+}
+
+int main(void) {
+    bool b = 1;
+    int x = 1;
+    int y = 2;
+
+    int v = select_u32(b, x, y);
+    printf("Value chosen was: %d\n", v);
+
+    return EXIT_SUCCESS;
+}
