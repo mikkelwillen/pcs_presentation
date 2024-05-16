58b407ffc6cee92acce0f853b120bb2fdf78e997
RasmusRoss <rnylandsted@gmail.com>
2024-05-16 12:53:42 +0200

    Changed library setup

--- constant_time_naive.c
+++ constant_time_naive.c
@@ -3,7 +3,16 @@
 # include <stdio.h>
 # include <stdint.h>
 
-uint32_t ct_select_u32(uint32_t x, uint32_t y) {
-    uint32_t m = -(uint32_t) ((x | -x) >> 31);
-    return (x & m) | (y & ~m);
+# include "mylib.h"
+
+int main(void) {
+    uint32_t x = 1;
+    uint32_t y = 2;
+    ct_select_u32(x, y);
+    return EXIT_SUCCESS;
 }
+
+// uint32_t ct_select_u32(uint32_t x, uint32_t y) {
+//     uint32_t m = -(uint32_t) ((x | -x) >> 31);
+//     return (x & m) | (y & ~m);
+// }
---
+++ mylib.h
@@ -0,0 +1,2 @@
+
+extern uint32_t ct_select_u32(uint32_t x, uint32_t y);
